2958e1e438e9bf0d8e0d6ade50f92ab9a7feb295
Matt Aimonetti <mattaimonetti@gmail.com>
2009-07-25 00:33:38 -0700

    improved views and added duplicates to index

--- lists/index.js
+++ lists/index.js
@@ -5,7 +5,7 @@ function(head, row, req, info) {
   // !code vendor/couchapp/date.js
   // !code vendor/couchapp/template.js
 
-  var indexPath = listPath('index','recent-logs',{descending:true, limit:5});
+  var indexPath = listPath('index','recent-logs',{descending:true, limit:50});
   // var feedPath = listPath('index','recent-logs',{descending:true, limit:5, format:"atom"});
   return respondWith(req, {
     html : function() { 
@@ -13,11 +13,13 @@ function(head, row, req, info) {
         return template(lib.templates.index.head, {title : 'Latest logs'})
       } else if (row) {
         var log = row.value;
+    
         return template(lib.templates.index.row, {
           timestamp : log.timestamp,
           url : log.url,
           duration : log.duration,
           amount_queries : log.amount_queries,
+          duplicates : log.amount_duplicates,
           link : showPath('log', row.id),
         });
       } else {
--- views/recent-logs/map.js
+++ views/recent-logs/map.js
@@ -1,8 +1,19 @@
 function(doc) {
-    emit(doc.started_at, {
-      timestamp : doc.started_at,
-      duration : doc.duration,
-      url : doc.url,
-      amount_queries : (doc['queries'] ? doc.queries.length : 0 )
-    });    
+  var duplicates = [];
+  if(doc['queries']){
+    var flattened_queries = doc.queries.map(function(query){ return (query['method'] + query['url'] + ((query['payload'] && query['payload']['keys']) ? query.payload.keys.join(', ') : '') ) }).sort();
+    for (var i = 0; i < flattened_queries.length - 1; i += 1) {
+      if (flattened_queries[i + 1] == flattened_queries[i]) {
+              duplicates.push(flattened_queries[i]);
+      };  
+    };
+  };
+  
+  emit(doc.started_at, {
+    timestamp : doc.started_at,
+    duration : doc.duration,
+    url : doc.url,
+    amount_queries : (doc['queries'] ? doc.queries.length : 0 ),
+    amount_duplicates : duplicates.length
+  });    
 };
